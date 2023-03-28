5ecf263256e7289df274b9ea6b65a4660887412d
João Matos <joaomarcosm.silva@outlook.com>
2023-03-28 18:04:30 -0300

    style: change logo text

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -3,7 +3,7 @@ import { Text } from "@chakra-ui/react";
 export function Logo() {
   return (
     <Text fontSize="3xl" fontWeight="normal" w="64">
-      myStock
+      Little Complete.
     </Text>
   );
 }
