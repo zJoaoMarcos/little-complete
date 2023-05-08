caaf492acb3935d5c206be91a20b4149e4319381
João Matos <joaomarcosm.silva@outlook.com>
2023-05-08 14:11:59 -0300

    create equipment avatar component

---
+++ src/components/Grids/EquipmentProfileGrid/EquipmentAvatar.tsx
@@ -0,0 +1,71 @@
+import { Avatar } from "@chakra-ui/react";
+import {
+  DesktopTower,
+  Laptop,
+  Monitor,
+  Phone,
+  Printer,
+  VirtualReality,
+} from "@phosphor-icons/react";
+
+interface EquipmentAvatarProps {
+  equipmentType: string;
+  iconSize: number;
+  avatarSize?: string;
+}
+
+export function EquipmentAvatar({
+  equipmentType,
+  avatarSize = "sm",
+  iconSize,
+}: EquipmentAvatarProps) {
+  if (equipmentType === "Desktop") {
+    return (
+      <Avatar
+        size={avatarSize}
+        bgColor="purple.400"
+        icon={<DesktopTower size={iconSize} />}
+      />
+    );
+  } else if (equipmentType === "Monitor") {
+    return (
+      <Avatar
+        size={avatarSize}
+        bgColor="purple.400"
+        icon={<Monitor size={iconSize} />}
+      />
+    );
+  } else if (equipmentType === "telephone") {
+    return (
+      <Avatar
+        size={avatarSize}
+        bgColor="purple.400"
+        icon={<Phone size={iconSize} />}
+      />
+    );
+  } else if (equipmentType === "Notebook") {
+    return (
+      <Avatar
+        size={avatarSize}
+        bgColor="purple.400"
+        icon={<Laptop size={iconSize} />}
+      />
+    );
+  } else if (equipmentType === "Vr") {
+    return (
+      <Avatar
+        size={avatarSize}
+        bgColor="purple.400"
+        icon={<VirtualReality size={iconSize} />}
+      />
+    );
+  } else if (equipmentType === "Scanner") {
+    return (
+      <Avatar
+        size={avatarSize}
+        bgColor="purple.400"
+        icon={<Printer size={iconSize} />}
+      />
+    );
+  }
+}
