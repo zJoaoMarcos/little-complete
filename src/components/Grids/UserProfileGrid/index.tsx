90662c1c912791b38bfb57835b3787d82637b5dd
João Matos <joaomarcosm.silva@outlook.com>
2023-05-09 14:43:42 -0300

    change to form grid

--- src/components/Grids/UserProfileGrid/index.tsx
+++ src/components/Grids/UserProfileGrid/index.tsx
@@ -1,13 +1,20 @@
-import { List, ListItem, SimpleGrid, Text } from "@chakra-ui/react";
-import {
-  BagSimple,
-  Calendar,
-  MicrosoftOutlookLogo,
-  Phone,
-  UserCircle,
-  UserCircleGear,
-  Wrench,
-} from "@phosphor-icons/react";
+import { Input } from "@/components/Form/input";
+import { useUser } from "@/contexts/UserContext";
+import { List, SimpleGrid } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type UpdateUserData = {
+  user_name: string;
+  complete_name: string;
+  title: string;
+  department_id: string;
+  telephone: number | null;
+  direct_boss: string;
+  smtp: string;
+  admission_date: Date | null;
+  demission_date: Date | null;
+  status: string;
+};
 
 interface UserProfileGridProps {
   user: {
@@ -18,138 +25,110 @@ interface UserProfileGridProps {
     telephone: number | null;
     direct_boss: string;
     smtp: string;
-    admission_date: string;
-    demission_date: string | null;
+    admission_date: Date | null;
+    demission_date: Date | null;
     status: string;
   };
+  isEditable: boolean;
 }
 
-export function UserProfileGrid({ user }: UserProfileGridProps) {
+export function UserProfileGrid({ user, isEditable }: UserProfileGridProps) {
+  const { register, handleSubmit, formState, reset } = useForm<UpdateUserData>({
+    defaultValues: {
+      user_name: user.user_name,
+      complete_name: user.complete_name,
+      title: user.title,
+      department_id: user.department_id,
+      telephone: user.telephone,
+      direct_boss: user.direct_boss,
+      smtp: user.smtp,
+      admission_date: user.admission_date,
+      demission_date: user.demission_date,
+      status: user.status,
+    },
+  });
+
+  const { isSubmitting } = formState;
+
+  const { updateUser } = useUser();
+
+  const handleUpdate: SubmitHandler<UpdateUserData> = async (data, event) => {
+    event.preventDefault();
+
+    await updateUser.mutateAsync(data);
+
+    reset();
+  };
+
   return (
-    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10} marginTop={8}>
+    <SimpleGrid
+      as="form"
+      id="update_user"
+      onSubmit={handleSubmit(handleUpdate)}
+      columns={{ base: 1, md: 2 }}
+      spacing={10}
+      marginTop={8}
+    >
       <List spacing={6}>
-        <ListItem
-          display="flex"
-          flexDir="row"
-          alignItems="center"
-          justifyContent="flex-start"
-          gap={1}
-        >
-          <UserCircle />
-          <Text as={"span"} fontWeight={"bold"}>
-            Usuário:
-          </Text>
+        <Input
+          size="md"
+          label="Usuário"
+          {...register("user_name")}
+          isDisabled={isEditable}
+        />
 
-          <Text pl="2">{user.user_name}</Text>
-        </ListItem>
+        <Input
+          size="md"
+          label="Cargo"
+          {...register("title")}
+          isDisabled={isEditable}
+        />
 
-        <ListItem
-          display="flex"
-          flexDir="row"
-          alignItems="center"
-          justifyContent="flex-start"
-          gap={1}
-        >
-          <Wrench />
-          <Text as={"span"} fontWeight={"bold"}>
-            Cargo:
-          </Text>
-          <Text>{user.title}</Text>
-        </ListItem>
+        <Input
+          size="md"
+          label="Departamento"
+          {...register("department_id")}
+          isDisabled={isEditable}
+        />
 
-        <ListItem
-          display="flex"
-          flexDir="row"
-          alignItems="center"
-          justifyContent="flex-start"
-          gap={1}
-        >
-          <BagSimple />
-          <Text as={"span"} fontWeight={"bold"}>
-            Departamento:
-          </Text>
-          <Text>{user.department_id}</Text>
-        </ListItem>
-
-        <ListItem
-          display="flex"
-          flexDir="row"
-          alignItems="center"
-          justifyContent="flex-start"
-          gap={1}
-        >
-          <UserCircleGear />
-          <Text as={"span"} fontWeight={"bold"}>
-            Chefia Imediata:
-          </Text>
-          <Text>{user.direct_boss}</Text>
-        </ListItem>
+        <Input
+          size="md"
+          label="Chefia Imediata"
+          {...register("direct_boss")}
+          isDisabled={isEditable}
+        />
       </List>
 
       <List spacing={6}>
-        <ListItem
-          display="flex"
-          flexDir="row"
-          alignItems="center"
-          justifyContent="flex-start"
-          gap={1}
-        >
-          <MicrosoftOutlookLogo />
-          <Text as={"span"} fontWeight={"bold"}>
-            E-mail:
-          </Text>
-          <Text>{user.smtp}</Text>
-        </ListItem>
+        <Input
+          size="md"
+          label="E-mail"
+          {...register("smtp")}
+          isDisabled={isEditable}
+        />
 
-        <ListItem
-          display="flex"
-          flexDir="row"
-          alignItems="center"
-          justifyContent="flex-start"
-          gap={1}
-        >
-          <Phone weight="fill" />
-          <Text as={"span"} fontWeight={"bold"}>
-            Ramal:
-          </Text>
-          <Text>{user.telephone}</Text>
-        </ListItem>
+        <Input
+          size="md"
+          label="Ramal"
+          {...register("telephone")}
+          isDisabled={isEditable}
+        />
 
-        <ListItem
-          display="flex"
-          flexDir="row"
-          alignItems="center"
-          justifyContent="flex-start"
-          gap={1}
-        >
-          <Calendar />
-          <Text as={"span"} fontWeight={"bold"}>
-            Data de Admissão:
-          </Text>
-          <Text>
-            {user.demission_date === null
-              ? " - / / - "
-              : String(user.demission_date)}
-          </Text>
-        </ListItem>
+        <Input
+          size="md"
+          label="Data de Admissão"
+          {...register("admission_date")}
+          type="date"
+          isDisabled={isEditable}
+        />
 
-        <ListItem
-          display="flex"
-          flexDir="row"
-          alignItems="center"
-          justifyContent="flex-start"
-          gap={1}
-        >
-          <Calendar />
-          <Text as={"span"} fontWeight={"bold"}>
-            Data de Demissão:
-          </Text>
-          <Text>
-            {user.demission_date === null
-              ? " - / / - "
-              : String(user.demission_date)}
-          </Text>
-        </ListItem>
+        <Input
+          size="md"
+          label="Data de Demissão"
+          {...register("demission_date")}
+          type="date"
+          isDisabled={isEditable}
+        />
       </List>
     </SimpleGrid>
   );
