82438d44dc6a47600540bf56e793d6cb1f668597
João Matos <joaomarcosm.silva@outlook.com>
2023-05-30 17:51:43 -0300

    update name in file

--- src/pages/departments/[id].tsx
+++ src/pages/departments/[id].tsx
@@ -1,6 +1,6 @@
 import { Layout } from "@/components/Layout";
 import { DepartmentProfile } from "@/components/Profiles/DepartmentProfile";
-import { getDepartment } from "@/hooks/UseFetchDepartment";
+import { getDepartment } from "@/hooks/UseFindDepartment";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { ReactElement } from "react";
