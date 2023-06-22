c612a21c17f7808a49fe0df1eaa6a3a17a4bc4e6
João Matos <joaomarcosm.silva@outlook.com>
2023-06-22 16:18:21 -0300

    create types file to component

--- src/components/Lists/EquipmentsList/EquipmentCard.tsx
+++ src/components/Lists/EquipmentsList/EquipmentCard.tsx
@@ -1,5 +1,3 @@
-import { EquipmentAvatar } from "@/components/Avatars/EquipmentAvatar";
-import { TriggerUnassignEquipment } from "@/components/Modals/UnassignEquipment/Trigger";
 import {
   Flex,
   HStack,
@@ -10,41 +8,9 @@ import {
 import { CaretRight } from "@phosphor-icons/react";
 import { useRouter } from "next/router";
 
-interface EquipmentCardProps {
-  equipment: {
-    id: string;
-    status: string;
-    currentUser: string | null;
-    patrimony: string | null;
-    type: string | null;
-    brand: string | null;
-    model: string | null;
-    serviceTag: string | null;
-    purchase: {
-      invoice: string | null;
-      supplier: string | null;
-      purchaseDate: Date | null;
-      warranty: string | null;
-    };
-    department: {
-      id: number | null;
-      name: string | null;
-    };
-    config: {
-      cpu: string | null;
-      ram: string | null;
-      video: string | null;
-      storage: {
-        slots: number | null;
-        storage0Type: string | null;
-        storage0Syze: number | null;
-        storage1Type: string | null;
-        storage1Syze: number | null;
-      };
-    };
-  };
-  buttonUnassign?: boolean;
-}
+import { EquipmentAvatar } from "@/components/Avatars/EquipmentAvatar";
+import { TriggerUnassignEquipment } from "@/components/Modals/UnassignEquipment/Trigger";
+import { EquipmentCardProps } from "./type";
 
 export function EquipmentCard({
   equipment,
---
+++ src/components/Lists/EquipmentsList/type.ts
@@ -0,0 +1,35 @@
+export interface EquipmentCardProps {
+  equipment: {
+    id: string;
+    status: string;
+    currentUser: string | null;
+    patrimony: string | null;
+    type: string | null;
+    brand: string | null;
+    model: string | null;
+    serviceTag: string | null;
+    purchase: {
+      invoice: string | null;
+      supplier: string | null;
+      purchaseDate: Date | null;
+      warranty: string | null;
+    };
+    department: {
+      id: number | null;
+      name: string | null;
+    };
+    config: {
+      cpu: string | null;
+      ram: string | null;
+      video: string | null;
+      storage: {
+        slots: number | null;
+        storage0Type: string | null;
+        storage0Syze: number | null;
+        storage1Type: string | null;
+        storage1Syze: number | null;
+      };
+    };
+  };
+  buttonUnassign?: boolean;
+}
