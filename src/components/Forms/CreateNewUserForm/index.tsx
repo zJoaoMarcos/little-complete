66ae254eb00d57e982462b7119a3596f75158561
João Matos <joaomarcosm.silva@outlook.com>
2023-07-03 09:22:47 -0300

    fix: move header form to index form component

--- src/components/Forms/CreateNewUserForm/index.tsx
+++ src/components/Forms/CreateNewUserForm/index.tsx
@@ -1,16 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { List, SimpleGrid } from "@chakra-ui/react";
+import {
+  Button,
+  Divider,
+  Flex,
+  HStack,
+  Heading,
+  List,
+  SimpleGrid,
+} from "@chakra-ui/react";
 
 import { Select } from "@/components/Form/Select";
 import { Input } from "@/components/Form/input";
-import { useEffect } from "react";
 import { UseCreateUser } from "./hooks/useCreateUser";
-import { NewUserFormProps } from "./types";
 
-export function CreateNewUserForm({
-  isSending,
-  setIsSending,
-}: NewUserFormProps) {
+export function CreateNewUserForm() {
   const {
     handleSubmit,
     register,
@@ -20,66 +23,82 @@ export function CreateNewUserForm({
     users,
   } = UseCreateUser();
 
-  useEffect(() => {
-    setIsSending(!isSending);
-  }, [isSubmitting]);
-
   return (
-    <SimpleGrid
-      as="form"
-      id="create_user"
-      onSubmit={handleSubmit(handleCreate)}
-      columns={{ base: 1, md: 2 }}
-      spacing={10}
-      marginTop={8}
-    >
-      <List spacing={4}>
-        <Input size="md" label="Usuário" {...register("user_name")} />
-
-        <Input size="md" label="E-mail" {...register("smtp")} />
-
-        <Input
-          size="md"
-          label="Ramal"
-          {...register("telephone")}
-          type="number"
-        />
-      </List>
-
-      <List spacing={4}>
-        <Input
-          size="md"
-          label="Nome Completo"
-          {...register("complete_name")}
-          type="text"
-        />
-
-        <Select
-          label="Departamento"
-          {...register("department_id")}
-          placeholder="Selecione o Departamento"
+    <>
+      <Flex mb="10" justify="space-between" align="center">
+        <HStack spacing={8}>
+          <Heading as="h3" fontWeight="semibold" fontSize={18}>
+            Novo Usuário
+          </Heading>
+        </HStack>
+        <Button
+          type="submit"
+          form="create_user"
+          colorScheme="purple"
+          isLoading={isSubmitting}
         >
-          {departments?.departments.map((department) => (
-            <option key={department.id} value={department.id}>
-              {department.name}
-            </option>
-          ))}
-        </Select>
+          Enviar
+        </Button>
+      </Flex>
 
-        <Input size="md" label="Cargo" {...register("title")} />
+      <Divider />
 
-        <Select
-          label="Chefia Imediata"
-          {...register("direct_boss")}
-          placeholder="Selecione a Chefia imediata"
-        >
-          {users?.users.map((user) => (
-            <option key={user.user_name} value={user.user_name}>
-              {user.user_name}
-            </option>
-          ))}
-        </Select>
-      </List>
-    </SimpleGrid>
+      <SimpleGrid
+        as="form"
+        id="create_user"
+        onSubmit={handleSubmit(handleCreate)}
+        columns={{ base: 1, md: 2 }}
+        spacing={10}
+        marginTop={8}
+      >
+        <List spacing={4}>
+          <Input size="md" label="Usuário" {...register("user_name")} />
+
+          <Input size="md" label="E-mail" {...register("smtp")} />
+
+          <Input
+            size="md"
+            label="Ramal"
+            {...register("telephone")}
+            type="number"
+          />
+        </List>
+
+        <List spacing={4}>
+          <Input
+            size="md"
+            label="Nome Completo"
+            {...register("complete_name")}
+            type="text"
+          />
+
+          <Select
+            label="Departamento"
+            {...register("department_id")}
+            placeholder="Selecione o Departamento"
+          >
+            {departments?.departments.map((department) => (
+              <option key={department.id} value={department.id}>
+                {department.name}
+              </option>
+            ))}
+          </Select>
+
+          <Input size="md" label="Cargo" {...register("title")} />
+
+          <Select
+            label="Chefia Imediata"
+            {...register("direct_boss")}
+            placeholder="Selecione a Chefia imediata"
+          >
+            {users?.users.map((user) => (
+              <option key={user.user_name} value={user.user_name}>
+                {user.user_name}
+              </option>
+            ))}
+          </Select>
+        </List>
+      </SimpleGrid>
+    </>
   );
 }
--- src/pages/users/new.tsx
+++ src/pages/users/new.tsx
@@ -1,38 +1,17 @@
 import { CreateNewUserForm } from "@/components/Forms/CreateNewUserForm";
 import { Layout } from "@/components/Layout";
-import { Button, Divider, Flex, HStack, Heading } from "@chakra-ui/react";
 import Head from "next/head";
-import { ReactElement, useState } from "react";
+import { ReactElement } from "react";
 import { NextPageWithLayout } from "../_app";
 
 const NewDepartment: NextPageWithLayout = () => {
-  const [isSending, setIsSending] = useState(false);
-
   return (
     <>
       <Head>
         <title>Novo usuário</title>
       </Head>
 
-      <Flex mb="10" justify="space-between" align="center">
-        <HStack spacing={8}>
-          <Heading as="h3" fontWeight="semibold" fontSize={18}>
-            Novo Usuário
-          </Heading>
-        </HStack>
-        <Button
-          type="submit"
-          form="create_user"
-          colorScheme="purple"
-          isLoading={isSending}
-        >
-          Enviar
-        </Button>
-      </Flex>
-
-      <Divider />
-
-      <CreateNewUserForm isSending={isSending} setIsSending={setIsSending} />
+      <CreateNewUserForm />
     </>
   );
 };
